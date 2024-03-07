d65ab4f6dd9ecdf752bdcdeeec9eba8c3615377f
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-08 00:18:31 +0800

    Update productcard.js
    A lot

--- productcard.js
+++ productcard.js
@@ -9,6 +9,287 @@ const ProductCard = ({ product }) => {
             <img src={product.image} alt={product.name} style={{ width: '100px' }} />
         </div>
     );
+    return (
+    <div>
+            <h3>{product.name2}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name3}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name4}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name5}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name6}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name7}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name8}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name9}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    );
+    return (
+    <div>
+            <h3>{product.name2}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name3}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name4}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name5}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name6}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name7}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name8}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name9}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    );
+    return (
+    <div>
+            <h3>{product.name2}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name3}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name4}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name5}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name6}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name7}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name8}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name9}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    );
+    return (
+    <div>
+            <h3>{product.name2}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name3}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name4}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name5}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name6}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name7}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name8}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+    return (
+        <div>
+            <h3>{product.name9}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} style={{ width: '100px' }} />
+        </div>
+    )
+
 };
 
 export default ProductCard;
